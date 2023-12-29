88fb06ed80be2e7e8de21f959a21fdc728706514
LeeHuan <1178228357@qq.com>
2023-12-29 07:12:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: '123 Main St',
+        skills: ['js', 'html', 'css']
+    };
+
+    // return the skills array
+    return member.skills;
+}
